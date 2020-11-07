3f238858e15511b0bc8271ace9a7a19f9b6348de
Wen-Chi Guo <campbellmath@gmail.com>
2020-11-07 21:58:37 +0800

    Vim

--- vim.sh
+++ vim.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env sh
 
-INSTALL_PATH=$HOME/opt/program/cgdb
-EXEC=$INSTALL_PATH/bin/cgdb
+INSTALL_PATH=$HOME/opt/program/vim
+EXEC=$INSTALL_PATH/bin/vim
 
 ###############################################################################
 # update git repositofy
 ###############################################################################
-git clone https://github.com/cgdb/cgdb
-cd cgdb
-sh autogen.sh
-cd ..
+git clone https://github.com/vim/vim
 
 ###############################################################################
 # build
 ###############################################################################
-rm -vfr build
-mkdir -p build
-cd build
-../cgdb/configure --prefix=$INSTALL_PATH
+cd vim
+export CFLAGS=-O3
+./configure                    \
+    --prefix=$INSTALL_PATH     \
+    --disable-gpm              \
+    --enable-perlinterp=yes    \
+    --enable-python3interp=yes \
+    --enable-cscope            \
+    --with-features=huge       \
+    --enable-multibyte         \
+    --enable-fontset           \
+    --enable-gui               \
+    --enable-gtk2-check
 make -srj4
 ###############################################################################
 # install
